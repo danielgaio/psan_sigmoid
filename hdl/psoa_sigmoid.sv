06eca70570591810ed9fe6c86b2d9f891707c2b6
Daniel <danielelielgaio@gmail.com>
2021-03-23 23:43:10 -0300

    Criados o projeto já estruturado com arquivos de fonte e teste

---
+++ hdl/psoa_sigmoid.sv
@@ -0,0 +1,15 @@
+// arquivo contendo a descricao sitetizavel
+
+`timescale 1ns / 1ps
+
+module psoa_sigmoid (
+	input 	logic [15:0] x,
+	input 	logic clk,
+	output 	logic [15:0] f_x
+);
+	
+	always @ (posedge clk) begin
+
+	end // always
+
+endmodule
